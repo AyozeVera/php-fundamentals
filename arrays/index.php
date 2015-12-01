8b3354270cedbceb6b89774aeb4b142f12aded76
Ayoze <ayo.verad@gmail.com>
2015-12-01 14:32:09 +0000

    first array example with 5 months

---
+++ arrays/index.php
@@ -0,0 +1,14 @@
+<?php
+  $months = array('january', 'february', 'march', 'april', 'may');
+  var_dump($months);
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Arrays</title>
+  </head>
+  <body>
+    <h1>Arrays</h1>
+  </body>
+</html>
