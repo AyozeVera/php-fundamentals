c1c706f3a80b6a2cb09d0ed877cb7a927e673350
Ayoze <ayo.verad@gmail.com>
2015-12-01 12:55:01 +0000

    two ways to embeed php on html

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My Website</title>
+  </head>
+  <body>
+    <h1>My Website</h1>
+    <?php
+      $name = "Ayoze Vera";
+     ?>
+      <p><?php echo "Hello $name"; ?></p>
+      <?php echo '<p>Hello '. $name . '</p>' ?>
+  </body>
+</html>
+
 <?php
-  $name = "Ayoze Vera";
-  echo "Hello $name";
-  echo 'Hello $name';
-  echo 'Hello ' . $name; 
