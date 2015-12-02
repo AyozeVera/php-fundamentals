0a59c18c6f3ff5968b2ffa34d563210f4cb41a21
Ayoze <ayo.verad@gmail.com>
2015-12-02 12:28:57 +0000

    sscanf saving on independent variables without list()

--- formatedstrings/index.php
+++ formatedstrings/index.php
@@ -1,7 +1,3 @@
 <?php
 
-list($month, $day, $year) = sscanf("June 7th, 2012", "%s %[^,], %d");
-//return an array
-echo $month;
-echo $day;
-echo $year;
+sscanf("June 7th, 2012", "%s %[^,], %d", $month, $day, $year);
