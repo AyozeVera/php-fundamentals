957e5b36d3f98af1b20f71c4712f2e642e776906
Ayoze <ayo.verad@gmail.com>
2015-12-02 12:00:48 +0000

    comment explaning the difference between include y require

--- includes/index.php
+++ includes/index.php
@@ -1,7 +1,10 @@
-<?php include 'inc/header.php' ?>
+<?php require 'inc/header.php' ?>
 
     <div class="container">
       <p>Main Content</p>
     </div>
 
 <?php include 'inc/footer.php' ?>
+
+//require shows a fatal error if the file is not found,
+//include shows just a warning and continue with execution
