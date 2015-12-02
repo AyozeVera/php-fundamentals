9e0c53cc272007328cc4a9151aaf72377d6dbaf0
Ayoze <ayo.verad@gmail.com>
2015-12-02 11:03:52 +0000

    using associative array

--- loops/index.php
+++ loops/index.php
@@ -1,7 +1,10 @@
 <?php
 
-$arr=['Ayo', 'Iban', 'Pablo', 'Domingo', 'Alex'];
-
-foreach ($arr as $name) {
-  echo $name;
+$arr = [
+  'dev' => 'Ayo',
+  'design' => 'Alex',
+  'ceo' => 'Domingo'
+];
+foreach ($arr as $title => $name) {
+  echo "<li><strong>$title</strong> -- $name</li>";
 }
