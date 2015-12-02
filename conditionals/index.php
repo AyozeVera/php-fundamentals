53be3010c769f6fa514359959ec7dda41e7d7f0a
Ayoze <ayo.verad@gmail.com>
2015-12-02 10:07:37 +0000

    elseif fixed

--- conditionals/index.php
+++ conditionals/index.php
@@ -4,7 +4,7 @@ $month = 'January';
 
 if ($month == 'January') { //=== checking data tipe, strict equality
   echo "It's Jan!";
-} else if($month == 'February') {
+} elseif($month == 'February') {
   echo "It's Feb!";
 }else{
   echo "Not the right month";
