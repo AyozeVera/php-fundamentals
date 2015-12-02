f5d61e8f93332bac07f35f5e2fd08eff9c325f91
Ayoze <ayo.verad@gmail.com>
2015-12-02 16:16:09 +0000

    the same using sprintf

--- heredocs/index.php
+++ heredocs/index.php
@@ -7,10 +7,6 @@ $post = [
   'publish-date'=>'6-10-2012'
 ];
 
-$email = "<h1>{$post['title']}</h1>";
-$email .= "<p> By: {$post['author']}</p>";
-$email .= "<div>{$post['body']}</div>";
-
 $email = sprintf("<h1>%s</h1><p>%s</p><div>%s</div>", $post['title'], $post['author'],$post['body']);
 
 echo $email;
