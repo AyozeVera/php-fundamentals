81a0afe85d3c1463dfe6475e8debed5506b822d1
Ayoze <ayo.verad@gmail.com>
2015-12-02 17:36:05 +0000

    using _GET to read the query string

---
+++ super-globals/about.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Super Globals</h1>
+    <?php echo $name ?>
+  </body>
+</html>
---
+++ super-globals/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<?php
+var_dump($_GET);
+?>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Super Globals</h1>
+    <?php echo $name ?>
+  </body>
+</html>
