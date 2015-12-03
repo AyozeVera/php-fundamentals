5286722ac3f5cc25d18921c27bac144d5f1a1231
Ayoze <ayo.verad@gmail.com>
2015-12-03 16:34:37 +0000

    pathinfo with extract

--- file-functions/index.php
+++ file-functions/index.php
@@ -2,8 +2,7 @@
 
   $images = glob('img/*.{jpg,png,jpeg}', GLOB_BRACE);
   foreach ($images as $image) {
-    $info = pathinfo($image);
-    echo $info['extension'];
-    echo "\r";
+    extract(pathinfo($image));
+    echo $extension . "<br>";
   }
  ?>
