b24854bb203a15917ad94961a64a7dd26002eeea
Ayoze <ayo.verad@gmail.com>
2015-12-03 16:26:38 +0000

    showin images

--- file-functions/index.php
+++ file-functions/index.php
@@ -1,6 +1,8 @@
 <?php
 
-  $files = glob('img/*.{jpg,png,jpeg}', GLOB_BRACE);
-  print_r($files);
-
+  $images = glob('img/*.{jpg,png,jpeg}', GLOB_BRACE);
+  foreach ($images as $image) {
+    echo $image;
+    echo "\r";
+  }
  ?>
