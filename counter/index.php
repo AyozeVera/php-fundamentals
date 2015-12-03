d3df16826e40f3b5b494466f8db8f476fa9cf855
Ayoze <ayo.verad@gmail.com>
2015-12-03 15:34:23 +0000

    use file to get an array from file

--- counter/index.php
+++ counter/index.php
@@ -2,9 +2,8 @@
 
 function set_count($file_name = 'counter.txt'){
   if (file_exists($file_name)) {
-    $count = (int)file_get_contents($file_name);
-    $count++;
-    file_put_contents($file_name, $count);
+    $array = file($file_name);
+    //file return an array setting keys with lines
   }else {
     $handle = fopen($file_name, 'w+');
     fwrite($handle, 1);
