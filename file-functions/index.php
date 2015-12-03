422dddf5643ba7eb815b6eb63805beac30b0cf57
Ayoze <ayo.verad@gmail.com>
2015-12-03 16:41:19 +0000

    fast fix

--- file-functions/index.php
+++ file-functions/index.php
@@ -4,8 +4,8 @@
   foreach ($images as $image) {
     //$info = pathinfo($image);
     //$thumb_name = $info['filename'] . '-thumb.' . $info['extension'];
-    extract(pathinfo(image));
-    $thumb_name = "$filename-thumb.$extension";
+    extract(pathinfo($image));
+    $thumb_name = "$filename.thumb.$extension";
     echo $thumb_name . '<br>';
   }
  ?>
