3a88c79496fa97bed2a85e483212ecee60dc3401
Ayoze <ayo.verad@gmail.com>
2015-12-04 11:05:52 +0000

    getting post values

---
+++ simple-email-registration/index.php
@@ -0,0 +1,14 @@
+<?php
+require 'functions.php'
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $name = trim($_POST['name']);
+  $email = trim($_POST['email']);
+
+  if (empty($name) || empty($email)) {
+    $status = 'Please provide a name and email';
+  }else {
+    add_register_user($username, $useremail);
+  }
+}
+
+include 'index.tmpl.php';
