5a083d0010a2b5fdad08e9dad1543dfd2508e0ec
Ayoze <ayo.verad@gmail.com>
2015-12-04 11:05:23 +0000

    view template

---
+++ simple-email-registration/index.tmpl.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style>
+      li {list-style: none;}
+    </style>
+  </head>
+  <body>
+    <h1>Join the Mailing List</h1>
+    <form class="" action="" method="post">
+      <?php if (isset($status)) echo $status; ?>
+      <ul>
+        <li><label for="name">Your Name:</label></li>
+        <li><input type="text" name="name" value=""></li>
+      </ul>
+      <ul>
+        <li><label for="email">Your Email:</label></li>
+        <li><input type="text" name="email" value=""></li>
+      </ul>
+      <ul>
+        <li><input type="submit" value="Sign Up"></li>
+      </ul>
+
+    </form>
+  </body>
+</html>
