6151a006710338c30d5efd14b16f8da7c7069419
Ayoze <ayo.verad@gmail.com>
2015-12-04 11:53:29 +0000

    mailing list variable refactor and creating get registered users function

--- simple-email-registration/functions.php
+++ simple-email-registration/functions.php
@@ -1,8 +1,9 @@
 <?php
 
+define('MAILING_LIST', 'mailing_list.php');
 function add_register_user($name, $email){
   // secure this file
-  file_put_contents('mailing_list.php', "$name: $email\n", FILE_APPEND );
+  file_put_contents(MAILING_LIST, "$name: $email\n", FILE_APPEND );
 }
 
 function old($key){
@@ -14,4 +15,8 @@ function old($key){
 
 function validate_email($email){
   return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
+function get_registered_users($path = MAILING_LIST){
+
 }
