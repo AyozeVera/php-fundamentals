cfa5eedb4f066e6c3b17d7119e4fab7c42378f1f
Ayoze <ayo.verad@gmail.com>
2015-12-09 14:44:11 +0000

    the same as before but using a variable

--- mysql-connect/functions.php
+++ mysql-connect/functions.php
@@ -2,15 +2,15 @@
 
 require 'config.php';
 
-$id = 2;
+$letter = 'A%';
 
 try {
 
   $conn = new PDO('mysql:host=127.0.0.1;dbname=practice', $config['DB_USERNAME'], $config['DB_PASSWORD']);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $stmt = $conn->prepare('SELECT * FROM users WHERE name LIKE "A%"');
-  $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+  $stmt = $conn->prepare('SELECT * FROM users WHERE name LIKE :letter');
+  $stmt->bindParam(':letter', $letter, PDO::PARAM_INT);
   $stmt->execute();
 
   while ($row = $stmt->fetch()) {
