a1bc3d133c678d57a3664be62611ec433feba25f
Ayoze <ayo.verad@gmail.com>
2015-12-09 13:01:55 +0000

    example of how to use an external variable in a query using a secure  way

--- mysql-connect/functions.php
+++ mysql-connect/functions.php
@@ -5,8 +5,9 @@ require 'config.php';
 try {
   $conn = new PDO('mysql:host=127.0.0.1;dbname=practice', $config['DB_USERNAME'], $config['DB_PASSWORD']);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  $results = $conn->query('SELECT * FROM users');
+  $id = 2;
+  $stmnt = $conn->prepare('SELECT FROM users WHERE id = :id')
+  $stmnt->execute(['id' =>$id]);
   foreach ($results as $row) {
     print_r($row);
   }
