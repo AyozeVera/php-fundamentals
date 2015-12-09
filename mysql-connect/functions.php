1f251be02bb4a44eb7d295f8a50242fa52b5b03a
Ayoze <ayo.verad@gmail.com>
2015-12-09 14:38:29 +0000

    al users.name starting with A

--- mysql-connect/functions.php
+++ mysql-connect/functions.php
@@ -9,7 +9,7 @@ try {
   $conn = new PDO('mysql:host=127.0.0.1;dbname=practice', $config['DB_USERNAME'], $config['DB_PASSWORD']);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $stmt = $conn->prepare('SELECT * FROM users WHERE id = :id');
+  $stmt = $conn->prepare('SELECT * FROM users WHERE name LIKE "A%"');
   $stmt->bindParam(':id', $id, PDO::PARAM_INT);
   $stmt->execute();
 
