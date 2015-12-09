01543a4ed8692a3c34a3b12b8ad58af80fcd4451
Ayoze <ayo.verad@gmail.com>
2015-12-09 12:58:22 +0000

    query example

--- mysql-connect/functions.php
+++ mysql-connect/functions.php
@@ -4,6 +4,12 @@ require 'config.php';
 
 try {
   $conn = new PDO('mysql:host=127.0.0.1;dbname=practice', $config['DB_USERNAME'], $config['DB_PASSWORD']);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $results = $conn->query('SELECT * FROM users');
+  foreach ($results as $row) {
+    print_r($row);
+  }
 } catch (PDOExpection $e) {
   echo 'Error: ' . $e->getMessage();
 }
