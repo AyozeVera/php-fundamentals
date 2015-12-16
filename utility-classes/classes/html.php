6099c6512277615338210c20e4511ecb861b0861
Ayoze <ayo.verad@gmail.com>
2015-12-16 14:40:41 +0000

    finishing the lessons with some funtionalities

--- utility-classes/classes/html.php
+++ utility-classes/classes/html.php
@@ -4,6 +4,12 @@ class HTML {
   public static function anchor($href, $text){
     return "<a href='$href'>$text</a>";
   }
-}
 
-echo HTML::anchor('http://google.es', 'Google');
+  public static function items($items){
+    return "<ul><li>" . implode('</li><li>', $items) . "</li></ul>";
+  }
+
+  public static function image($src, $alt = ''){
+    return "<img src='$src' alt='$alt'>";
+  }
+}
