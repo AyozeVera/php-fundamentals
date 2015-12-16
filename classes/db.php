10a08a544f23238a68f4cda7e727ccba4c6584df
Ayoze <ayo.verad@gmail.com>
2015-12-16 11:38:19 +0000

    diferent ways to declare a property

--- classes/db.php
+++ classes/db.php
@@ -1,9 +1,15 @@
 <?php
 
-class Person
+class House
 {
+  public $home_color = 'white';
+  // private $home_color = 'white';
+  // protected $home_color = 'white';
   public function __construct(){
     echo 'Running';
   }
 
 }
+
+$house = new House();
+echo $house->home_color;
