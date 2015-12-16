78df7370be3bd59e58219c24f5ab6b52048b3120
Ayoze <ayo.verad@gmail.com>
2015-12-16 11:51:35 +0000

    creating house with no parameter

--- classes/db.php
+++ classes/db.php
@@ -4,8 +4,10 @@ class House {
   public $home_color = 'white';
   public $rooms = 3;
 
-  public function __construct($home_color){
-    $this->hombe_color = $home_color;
+  public function __construct($home_color = null){
+    if ($home_color) {
+      $this->hombe_color = $home_color;
+    }
   }
 
   public function add_room(){
@@ -17,5 +19,16 @@ class House {
 $house = new House('red');
 $house->add_room();
 $house->add_room();
-
 echo "This {$house->home_color} house has {$house->rooms} rooms.";
+
+echo "<br>";
+
+$house2 =  new House();
+$house2->add_room();
+$house2->add_room();
+$house2->add_room();
+$house2->add_room();
+$house2->add_room();
+$house2->add_room();
+$house2->add_room();
+echo "This {$house2->home_color} house has {$house2->rooms} rooms.";
